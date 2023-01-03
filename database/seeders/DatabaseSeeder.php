e5a8a0f15c48e09d20f872891c97691a2a47f00f
Omar Cayam <marcvegasprv@protonmail.com>
2023-01-03 23:38:36 +0800

    Updated dependencies to Laravel 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
-
+ 
+namespace Database\Seeders;
+ 
 use Illuminate\Database\Seeder;
-
+ 
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        
     }
 }
